ae99f2c7d72ea48534d067ca47f87a24f7576097
Avinash Singh <avinash06nitp@gmail.com>
2026-05-01 13:55:30 +0530

    Revert "Merge pull request #13 from avi06nitp/feature/avi06nitp/tic-tac-toe"
    This reverts commit 38d156a3d52fae85c206c4f185aa3077fd0abfca.

--- tic-tac-toe/exceptions/AlreadyOccupiedCellException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class AlreadyOccupiedCellException extends InvalidInputExeption {
-    public AlreadyOccupiedCellException(int row, int column) {
-        super("Provided cell at row:"+row +" column: "+column +" is already occupied");
-    }
-}
--- tic-tac-toe/exceptions/InvalidInputExeption.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidInputExeption extends RuntimeException {
-    public InvalidInputExeption(String message) {
-        super(message);
-    }
-}
