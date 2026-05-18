9639a22a5247a7c015f1e2ef94e138409cb0e9d8
Avinash Singh <avinash06nitp@gmail.com>
2026-05-18 21:00:13 +0530

    fix(library-management): correct typo'd import and drop unused import

--- library-management/LibraryManagementSystem.java
+++ library-management/LibraryManagementSystem.java
@@ -14,7 +14,7 @@ import librarymanagement.registry.UserRegistry;
 import librarymanagement.service.FeeCalculationService;
 import librarymanagement.service.RentalService;
 
-kimport java.time.Clock;
+import java.time.Clock;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
--- library-management/librarymanagement/notification/OverdueObserver.java
+++ library-management/librarymanagement/notification/OverdueObserver.java
@@ -1,6 +1,5 @@
 package librarymanagement.notification;
 
-import librarymanagement.models.RentedBooks;
 
 public interface OverdueObserver {
     void onOverdue(OverdueEvent overdueEvent);
